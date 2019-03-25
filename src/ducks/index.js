84eed137cd6529e7c421a9f3e4689a04327df25b
NickDoychev <NickDoychev>
2019-03-25 16:11:04 +0200

    add redux-logger

--- src/ducks/index.js
+++ src/ducks/index.js
@@ -1,11 +1,15 @@
 import {combineReducers, createStore, applyMiddleware} from "redux";
 import thunk from "redux-thunk";
+import { createLogger } from 'redux-logger'
 
 import {reducer as moviesReducer} from "./MoviesReducer";
 
+const logger = createLogger({
+	// ...options
+});
 
 const reducer = combineReducers({
 	moviesReducer,
 });
 
-export const store = createStore(reducer, applyMiddleware(thunk));
+export const store = createStore(reducer, applyMiddleware(logger, thunk));
