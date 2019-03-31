aeebcffa2841cc273106bb0fefcff46209030d90
NickDoychev <NickDoychev>
2019-04-01 00:52:32 +0300

    Updates

---
+++ precache-manifest.7dc5f49cfd837a3066cd91c4813e08bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d74f84433da631d57c20",
+    "url": "/homework/static/css/main.3d99febe.chunk.css"
+  },
+  {
+    "revision": "d74f84433da631d57c20",
+    "url": "/homework/static/js/main.85c3cdbf.chunk.js"
+  },
+  {
+    "revision": "6709aa0db1aacc8674fd",
+    "url": "/homework/static/js/runtime~main.8eef0389.js"
+  },
+  {
+    "revision": "4e97fd1e05840bb67269",
+    "url": "/homework/static/js/2.f07b7193.chunk.js"
+  },
+  {
+    "revision": "bfed49dcb81d4fa787e627fa80ad7026",
+    "url": "/homework/index.html"
+  }
+];
